0d1e3e1784510662c04822db1735d941ac117198
glihm <dev@glihm.net>
2023-09-06 22:21:42 -0600

    add CairoAbiEntry

--- crates/cairo-type-parser/src/lib.rs
+++ crates/cairo-type-parser/src/lib.rs
@@ -4,48 +4,32 @@ use proc_macro2::TokenStream as TokenStream2;
 use std::collections::HashMap;
 use syn::Type;
 
-pub enum TokenType {
-    Unit,
-    Felt,
-    Array(Box<ArrayToken>),
-    Tuple(TupleToken),
-    Struct(StructToken),
-    Enum(EnumToken),
-    Function(FunctionToken),
+use abi_type::AbiType;
+
+pub enum CairoFunctionStateMutability {
+    View,
+    External,
 }
 
-pub struct ArrayToken {
-    inner: TokenType,
+pub enum CairoAbiEntry {
+    Struct(CairoStruct),
+    Enum(CairoEnum),
+    Function(CairoFunction),
 }
 
-pub struct TupleToken {
-    values: Vec<TokenType>,
+pub struct CairoStruct {
+    pub name: AbiType,
+    pub members: Vec<(String, AbiType)>,
 }
 
-pub struct StructToken {
-    name: String,
-    members: Vec<(String, TokenType)>,
+pub struct CairoEnum {
+    pub name: AbiType,
+    pub variants: Vec<(String, AbiType)>,
 }
 
-pub struct EnumToken {
-    name: String,
-    variants: Vec<(String, TokenType)>,
-}
-
-pub struct FunctionToken {
-    name: String,
-    inputs: Vec<(String, TokenType)>,
-    outputs: Vec<TokenType>,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_1() {
-    }
-
-    #[test]
-    fn test_tokenize_simple() {}
+pub struct CairoFunction {
+    pub name: AbiType,
+    pub state_mutability: CairoFunctionStateMutability,
+    pub inputs: Vec<(String, AbiType)>,
+    pub outputs: Vec<AbiType>,
 }
