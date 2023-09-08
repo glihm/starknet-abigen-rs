43b55af3e61a6c6f2922e1f5a4af6b534718c9fc
Pia <76558220+rkdud007@users.noreply.github.com>
2023-09-08 20:46:05 +0900

    signed integer macro

--- crates/cairo-types/src/types/data_type.rs
+++ crates/cairo-types/src/types/data_type.rs
@@ -20,12 +20,35 @@ macro_rules! implement_trait_for_unsigned {
     };
 }
 
+macro_rules! implement_trait_for_signed {
+    ($type:ty) => {
+        impl CairoType for $type {
+            type RustType = Self;
+
+            fn serialize(rust: &Self::RustType) -> Vec<FieldElement> {
+                vec![FieldElement::from(*rust as usize)]
+            }
+
+            fn deserialize(felts: &[FieldElement], offset: usize) -> Result<Self::RustType> {
+                let temp: u16 = felts[offset].try_into().unwrap();
+                Ok(temp as $type)
+            }
+        }
+    };
+}
+
 implement_trait_for_unsigned!(u8);
 implement_trait_for_unsigned!(u16);
 implement_trait_for_unsigned!(u32);
 implement_trait_for_unsigned!(u64);
 implement_trait_for_unsigned!(u128);
 implement_trait_for_unsigned!(usize);
+
+implement_trait_for_signed!(i8);
+implement_trait_for_signed!(i16);
+implement_trait_for_signed!(i32);
+implement_trait_for_signed!(i64);
+implement_trait_for_signed!(i128);
 
 impl CairoType for FieldElement {
     type RustType = Self;
@@ -261,6 +284,81 @@ mod tests {
         let felts = vec![FieldElement::from(123_u128), FieldElement::from(99_u128)];
         assert_eq!(u128::deserialize(&felts, 0).unwrap(), 123);
         assert_eq!(u128::deserialize(&felts, 1).unwrap(), 99);
+    }
+
+    #[test]
+    fn test_serialize_i8() {
+        let v = 123_i8;
+        let felts = i8::serialize(&v);
+        assert_eq!(felts.len(), 1);
+        assert_eq!(felts[0], FieldElement::from(123 as u8));
+    }
+
+    #[test]
+    fn test_deserialize_i8() {
+        let felts = vec![FieldElement::from(123 as u8), FieldElement::from(123 as u8)];
+        assert_eq!(i8::deserialize(&felts, 0).unwrap(), 123);
+        assert_eq!(i8::deserialize(&felts, 1).unwrap(), 123);
+    }
+
+    #[test]
+    fn test_serialize_i16() {
+        let v = 123_i16;
+        let felts = i16::serialize(&v);
+        assert_eq!(felts.len(), 1);
+        assert_eq!(felts[0], FieldElement::from(123 as u8));
+    }
+
+    #[test]
+    fn test_deserialize_i16() {
+        let felts = vec![FieldElement::from(123 as u8), FieldElement::from(123 as u8)];
+        assert_eq!(i16::deserialize(&felts, 0).unwrap(), 123);
+        assert_eq!(i16::deserialize(&felts, 1).unwrap(), 123);
+    }
+
+    #[test]
+    fn test_serialize_i32() {
+        let v = 123_i32;
+        let felts = i32::serialize(&v);
+        assert_eq!(felts.len(), 1);
+        assert_eq!(felts[0], FieldElement::from(123 as u8));
+    }
+
+    #[test]
+    fn test_deserialize_i32() {
+        let felts = vec![FieldElement::from(123 as u8), FieldElement::from(123 as u8)];
+        assert_eq!(i32::deserialize(&felts, 0).unwrap(), 123);
+        assert_eq!(i32::deserialize(&felts, 1).unwrap(), 123);
+    }
+
+    #[test]
+    fn test_serialize_i64() {
+        let v = 123_i64;
+        let felts = i64::serialize(&v);
+        assert_eq!(felts.len(), 1);
+        assert_eq!(felts[0], FieldElement::from(123 as u8));
+    }
+
+    #[test]
+    fn test_deserialize_i64() {
+        let felts = vec![FieldElement::from(123 as u8), FieldElement::from(123 as u8)];
+        assert_eq!(i64::deserialize(&felts, 0).unwrap(), 123);
+        assert_eq!(i64::deserialize(&felts, 1).unwrap(), 123);
+    }
+
+    #[test]
+    fn test_serialize_i128() {
+        let v = 123_i128;
+        let felts = i128::serialize(&v);
+        assert_eq!(felts.len(), 1);
+        assert_eq!(felts[0], FieldElement::from(123 as u8));
+    }
+
+    #[test]
+    fn test_deserialize_i128() {
+        let felts = vec![FieldElement::from(123 as u8), FieldElement::from(123 as u8)];
+        assert_eq!(i128::deserialize(&felts, 0).unwrap(), 123);
+        assert_eq!(i128::deserialize(&felts, 1).unwrap(), 123);
     }
 
     #[test]
