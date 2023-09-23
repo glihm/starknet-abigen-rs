eefa20a59cdd3fcb023162f8b86ca263b7f98556
glihm <dev@glihm.net>
2023-09-22 23:39:21 -0600

    structure main for example

---
+++ src/autogen_abis/basic_abi.rs
@@ -0,0 +1,59 @@
+// WARNING: This file is auto-generated.
+
+use abigen_macro::abigen;
+use cairo_types::CairoType;
+use starknet::core::types::{BlockId, BlockTag};
+use starknet::providers::Provider;
+use starknet::accounts::Account;
+abigen!(BasicContract,
+r#"
+[
+  {
+    "type": "struct",
+    "name": "core::integer::u256",
+    "members": [
+      {
+        "name": "low",
+        "type": "core::integer::u128"
+      },
+      {
+        "name": "high",
+        "type": "core::integer::u128"
+      }
+    ]
+  },
+  {
+    "type": "function",
+    "name": "set_storage",
+    "inputs": [
+      {
+        "name": "v1",
+        "type": "core::felt252"
+      },
+      {
+        "name": "v2",
+        "type": "core::integer::u256"
+      }
+    ],
+    "outputs": [],
+    "state_mutability": "external"
+  },
+  {
+    "type": "function",
+    "name": "read_storage_tuple",
+    "inputs": [],
+    "outputs": [
+      {
+        "type": "(core::felt252, core::integer::u256)"
+      }
+    ],
+    "state_mutability": "view"
+  },
+  {
+    "type": "event",
+    "name": "contracts::basic::basic::Event",
+    "kind": "enum",
+    "variants": []
+  }
+]
+"#);
